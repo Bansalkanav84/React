a0b1aaaf7092e0b3545b9eb6960b0ebc59622a64
Kanav Bansal <bansalkanav84@gmail.com>
2025-06-23 18:19:08 +0530

    Currency Converter

---
+++ currency-converter/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,19 @@
+import {useEffect, useState} from "react"
+
+
+function useCurrencyInfo(currency){
+    const [data, setData] = useState({})
+    useEffect(() => {
+        fetch(`https://api.exchangerate-api.com/v4/latest/${currency}`)
+        .then((res) => res.json())
+        .then((res) => setData(res.rates))
+    }, [currency])
+
+    useEffect(() => {
+        console.log(data);
+    }, [data]);
+
+    return data
+}
+
+export default useCurrencyInfo;
